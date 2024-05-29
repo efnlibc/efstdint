9dc46151c295fde4b3606c714f3b6d76bf4f01e3
Efgeen <efgeen1998@gmail.com>
2024-05-29 22:20:00 +0200

    .

---
+++ efstdint.h
@@ -0,0 +1,12 @@
+#ifndef _EFSTDINT_H
+#define _EFSTDINT_H
+
+typedef char int8_t;
+typedef unsigned char uint8_t;
+typedef char int_least8_t;
+typedef unsigned char uint_least8_t;
+typedef char int_fast8_t;
+typedef unsigned char uint_fast8_t;
+
+#endif
+
